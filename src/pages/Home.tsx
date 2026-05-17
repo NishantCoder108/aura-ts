922460be93fd40b8c91a18ef77b239cd49b00bb9
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-05-17 12:36:10 +0530

    chore: optimized code

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import reactLogo from "../assets/react.svg";
+import viteLogo from "../assets/vite.svg";
+import heroImg from "../assets/hero.png";
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+  return <>div</>;
+}
